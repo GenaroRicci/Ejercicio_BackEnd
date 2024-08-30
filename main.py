448d3d7cd72526fa838bf6f04ee936f6220b04ce
Genaro Ricci <genaroricci2@gmail.com>
2024-08-29 23:41:02 -0300

    Seguir ma;ana temprano

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 
-# *Empezando el proyecto
+# Detalles a tener en cuenta de la preparacion.
 
+# * Info importante resaltada
+# ! Peligro
+# ? Cuestionamiento, utilizar o no?
+# TODO : Cosas para hacer eventualmente.
+
+from flask import Flask
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
