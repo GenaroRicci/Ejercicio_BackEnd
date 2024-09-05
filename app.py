4ed6b2ef76eefd3703caad7ca3531c4af8416f9f
Genaro Ricci <genaroricci2@gmail.com>
2024-09-04 23:24:59 -0300

    Ojo con lo que miras Asado corneta

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
+
+# * Info importante resaltada
+# ! Peligro
+# ? Cuestionamiento, utilizar o no?
+# TODO : Cosas para hacer eventualmente.
+
+# * Importaciones
 from flask import Flask
+from authentication import input_data
+
+# TODO from markupsafe import escape
 
 app = Flask(__name__)
 @app.route("/")
